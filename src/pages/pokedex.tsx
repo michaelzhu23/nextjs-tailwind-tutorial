b6bf880bdc02948f81b2da149f40c65ca0908dd9
Michael Zhu <mzhu23@gmail.com>
2022-09-14 05:04:07 -0700

    Updates

--- src/pages/pokedex.tsx
+++ src/pages/pokedex.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import PokedexLogo from "./images/pokedex.svg";
 import PokedexLogoNew from "./images/pokedex2022.png";
 
@@ -18,43 +19,49 @@ const pokedex = () => {
     }
   };
 
-  const handleOnChange = (event) => {
+  const handleOnChange = (event: { target: { value: string; }; }) => {
     const value = event.target.value;
     setInput(value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
     fetchPokemonData(input);
     setInput("");
   };
 
   return (
-    <div className="flex flex-col items-center mt-10 px-5">
-      <Image src={PokedexLogo} width={400} height={200}/>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="border-2 border-black border-solid rounded mt-2"
-          type="text"
-          value={input}
-          onChange={handleOnChange}
+    <>
+      <Link href="/">
+        <a className="text-blue-500 hover:underline block m-10">&lt; Back to home!</a>
+      </Link>
+      <div className="flex flex-col items-center mt-10 px-5">
+        <Image src={PokedexLogo} width={400} height={200} />
+        <form onSubmit={handleSubmit}>
+          <input
+            className="border-2 border-black border-solid rounded mt-2"
+            type="text"
+            value={input}
+            onChange={handleOnChange}
+            placeholder="Search for a Pokemon!"
+          />
+        </form>
+        <h2 className="mt-5 text-blue-600 text-xl">
+          {apiData?.name.toUpperCase()}
+        </h2>
+        <img
+          className="my-8"
+          src={apiData?.sprites.other.dream_world.front_default}
         />
-      </form>
-      <h2 className="mt-5 text-blue-600 text-xl">
-        {apiData?.name.toUpperCase()}
-      </h2>
-      <img
-        className="my-8"
-        src={apiData?.sprites.other.dream_world.front_default}
-      />
-      <ol className="list-decimal">
-        {apiData?.moves.slice(0, 5).map((move, index) => (
-          <li style={{ fontWeight: "bold" }} key={index}>
-            {move.move.name}
-          </li>
-        ))}
-      </ol>
-    </div>
+        <ol className="list-decimal">
+          {apiData?.moves.slice(0, 5).map((move, index) => (
+            <li style={{ fontWeight: "bold" }} key={index}>
+              {move.move.name}
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
   );
 };
 
